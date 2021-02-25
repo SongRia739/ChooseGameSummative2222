43d8e10c5a0d4da2c026f8debc291bde2422d6a8
Ria Das <riadas739@ed.amdsb.ca>
2021-02-25 10:48:19 -0500

    Add project files.

---
+++ ChooseGameSummative2222/Form1.Designer.cs
@@ -0,0 +1,161 @@
+﻿namespace ChooseGameSummative2222
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.heartBox = new System.Windows.Forms.PictureBox();
+            this.starBox = new System.Windows.Forms.PictureBox();
+            this.diamondBox = new System.Windows.Forms.PictureBox();
+            this.diamondLabel = new System.Windows.Forms.Label();
+            this.starLabel = new System.Windows.Forms.Label();
+            this.heartLabel = new System.Windows.Forms.Label();
+            this.textLabel = new System.Windows.Forms.Label();
+            this.sceneImage = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.heartBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.starBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.diamondBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.sceneImage)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // heartBox
+            // 
+            this.heartBox.BackColor = System.Drawing.Color.Transparent;
+            this.heartBox.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("heartBox.BackgroundImage")));
+            this.heartBox.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.heartBox.Location = new System.Drawing.Point(311, 271);
+            this.heartBox.Name = "heartBox";
+            this.heartBox.Size = new System.Drawing.Size(60, 33);
+            this.heartBox.TabIndex = 18;
+            this.heartBox.TabStop = false;
+            // 
+            // starBox
+            // 
+            this.starBox.BackColor = System.Drawing.Color.Transparent;
+            this.starBox.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("starBox.BackgroundImage")));
+            this.starBox.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.starBox.Location = new System.Drawing.Point(311, 324);
+            this.starBox.Name = "starBox";
+            this.starBox.Size = new System.Drawing.Size(60, 33);
+            this.starBox.TabIndex = 17;
+            this.starBox.TabStop = false;
+            // 
+            // diamondBox
+            // 
+            this.diamondBox.BackColor = System.Drawing.Color.Transparent;
+            this.diamondBox.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("diamondBox.BackgroundImage")));
+            this.diamondBox.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.diamondBox.Location = new System.Drawing.Point(311, 387);
+            this.diamondBox.Name = "diamondBox";
+            this.diamondBox.Size = new System.Drawing.Size(60, 33);
+            this.diamondBox.TabIndex = 16;
+            this.diamondBox.TabStop = false;
+            // 
+            // diamondLabel
+            // 
+            this.diamondLabel.ForeColor = System.Drawing.Color.Black;
+            this.diamondLabel.Location = new System.Drawing.Point(380, 397);
+            this.diamondLabel.Name = "diamondLabel";
+            this.diamondLabel.Size = new System.Drawing.Size(194, 23);
+            this.diamondLabel.TabIndex = 15;
+            this.diamondLabel.Text = "Space Key";
+            // 
+            // starLabel
+            // 
+            this.starLabel.ForeColor = System.Drawing.Color.Black;
+            this.starLabel.Location = new System.Drawing.Point(377, 334);
+            this.starLabel.Name = "starLabel";
+            this.starLabel.Size = new System.Drawing.Size(194, 23);
+            this.starLabel.TabIndex = 14;
+            this.starLabel.Text = "N key";
+            // 
+            // heartLabel
+            // 
+            this.heartLabel.BackColor = System.Drawing.Color.Transparent;
+            this.heartLabel.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.heartLabel.Location = new System.Drawing.Point(377, 281);
+            this.heartLabel.Name = "heartLabel";
+            this.heartLabel.Size = new System.Drawing.Size(197, 23);
+            this.heartLabel.TabIndex = 13;
+            this.heartLabel.Text = "M key";
+            // 
+            // textLabel
+            // 
+            this.textLabel.BackColor = System.Drawing.Color.White;
+            this.textLabel.Location = new System.Drawing.Point(308, 12);
+            this.textLabel.Name = "textLabel";
+            this.textLabel.Size = new System.Drawing.Size(253, 200);
+            this.textLabel.TabIndex = 12;
+            // 
+            // sceneImage
+            // 
+            this.sceneImage.BackColor = System.Drawing.Color.White;
+            this.sceneImage.Location = new System.Drawing.Point(12, 12);
+            this.sceneImage.Name = "sceneImage";
+            this.sceneImage.Size = new System.Drawing.Size(273, 426);
+            this.sceneImage.TabIndex = 11;
+            this.sceneImage.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.PaleTurquoise;
+            this.ClientSize = new System.Drawing.Size(581, 450);
+            this.Controls.Add(this.heartBox);
+            this.Controls.Add(this.starBox);
+            this.Controls.Add(this.diamondBox);
+            this.Controls.Add(this.diamondLabel);
+            this.Controls.Add(this.starLabel);
+            this.Controls.Add(this.heartLabel);
+            this.Controls.Add(this.textLabel);
+            this.Controls.Add(this.sceneImage);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            ((System.ComponentModel.ISupportInitialize)(this.heartBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.starBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.diamondBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.sceneImage)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox heartBox;
+        private System.Windows.Forms.PictureBox starBox;
+        private System.Windows.Forms.PictureBox diamondBox;
+        private System.Windows.Forms.Label diamondLabel;
+        private System.Windows.Forms.Label starLabel;
+        private System.Windows.Forms.Label heartLabel;
+        private System.Windows.Forms.Label textLabel;
+        private System.Windows.Forms.PictureBox sceneImage;
+    }
+}
+
---
+++ ChooseGameSummative2222/Form1.cs
@@ -0,0 +1,276 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Media;
+
+namespace ChooseGameSummative2222
+{
+    public partial class Form1 : Form
+    {
+        // tracks what part of the game the user is at
+        int scene = 0;
+
+        // random number generator
+        Random randGen = new Random();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.M)       //heart button press
+            {
+                if (scene == 0) { scene = 1; }
+                else if (scene == 1) { scene = 3; }
+                else if (scene == 3) { scene = 5; }
+                else if (scene == 5) { scene = 99; }
+                else if (scene == 6) { scene = 9; }
+                else if (scene == 9) { scene = 14; }
+                else if (scene == 14) { scene = 99; }
+                else if (scene == 15) { scene = 18; }
+                else if (scene == 18) { scene = 99; }
+                else if (scene == 2) { scene = 4; }
+                else if (scene == 4) { scene = 7; }
+                else if (scene == 7) { scene = 13; }
+                else if (scene == 12) { scene = 17; }
+                else if (scene == 16) { scene = 22; }
+                else if (scene == 99) { scene = 0; }
+
+            }
+            else if (e.KeyCode == Keys.N) //star button press
+            {
+                if (scene == 6)
+                {
+                    int value = randGen.Next(1, 11);
+
+                    if (value <= 3) { scene = 10; }
+                    else { scene = 11; }
+                }
+                else if (scene == 11) { scene = 99; }
+                else if (scene == 0) { scene = 2; }
+                else if (scene == 2) { scene = 0; }
+                else if (scene == 4) { scene = 8; }
+                else if (scene == 8) { scene = 99; }
+                else if (scene == 7) { scene = 12; }
+                else if (scene == 12) { scene = 16; }
+                else if (scene == 16) { scene = 23; }
+                else if (scene == 23) { scene = 99; }
+                else if (scene == 1) { scene = 0; }
+                else if (scene == 3) { scene = 6; }
+                else if (scene == 15) { scene = 19; }
+
+            }
+            else if (e.KeyCode == Keys.Space) //diamond button press
+            {
+                if (scene == 15) { scene = 20; }
+                else if (scene == 20) { scene = 99; }
+                else if (scene == 16) { scene = 21; }
+                else if (scene == 21) { scene = 99; }
+            }
+
+            /// Display text and game options to screen based on the current scene
+            switch (scene)
+            {
+                case 0:
+                    SoundPlayer opening = new SoundPlayer(Properties.Resources.opening);
+                    opening.Play();
+                    textLabel.Text = "You just got of class at your 3rd year in university. Walking around campus, you notice 2 boys. Which one do you have a crush on? ";
+                    heartLabel.Text = "Bang 'Christopher' Chan";
+                    starLabel.Text = "Seo Changbin";
+                    diamondLabel.Text = "";
+                    break;
+                case 1:
+           //         SoundPlayer chrisTheme = new SoundPlayer(Properties.Resources.resturantChatterrr);
+           //         chrisTheme.Play();
+                    textLabel.Text = "Bang 'Christopher' Chan- Very popular, extremely nice, friends with basically everyone on campus and a bussniss major. Do you like him?";
+                    heartLabel.Text = "Yes";
+                    starLabel.Text = "No, not my type";
+                    diamondLabel.Text = "";
+                    break;
+                case 2:
+                    SoundPlayer changbinTheme = new SoundPlayer(Properties.Resources.rock);
+                    changbinTheme.Play();
+                    textLabel.Text = "Seo Changbin- everyone knows him, bad boy persona, player, and is here on a sports scholarship. Do you like him?";
+                    heartLabel.Text = "Yes";
+                    starLabel.Text = "No, not my type";
+                    diamondLabel.Text = "";
+                    break;
+                case 3:
+                    textLabel.Text = "Chris notices you and quickly gathers up his things before jogging up to you. He asks if you're busy, are you?";
+                    heartLabel.Text = "Yes";
+                    starLabel.Text = "No!";
+                    diamondLabel.Text = "";
+                    break;
+                case 4:
+                    textLabel.Text = "You shyly approach him and ask if he want's to hang out with you. He scoffs but says yes anyway. Strange he agreed so fast but you think nothing of it. You hear whispers as the two of you get off campus. They call you his next use and throw lover. What do you do?";
+                    heartLabel.Text = "Roll your eyes and glare at them ";
+                    starLabel.Text = "Shamefully lower your head even though it isn't true";
+                    diamondLabel.Text = "";
+                    break;
+                case 5:
+                     SoundPlayer gameOver = new SoundPlayer(Properties.Resources.gameover);
+                     gameOver.Play();
+                    textLabel.Text = "You say yes and he laughs and bids a somewhat awkward goodbye, you lost your chance.";
+                    heartLabel.Text = "";
+                    starLabel.Text = "";
+                    diamondLabel.Text = "";
+                    break;
+                case 6:
+                    textLabel.Text = "His smile grows wider as he invites you out for lunch. You agree because duh, who wouldn't? The two of you walk off campus and end up between two shops. ";
+                    heartLabel.Text = "The sushi place";
+                    starLabel.Text = "McDonalds";
+                    diamondLabel.Text = "";
+                    break;
+                case 7:
+                    textLabel.Text = "Changbin smirks and stands a little closer to you, the walking eventually leads you guys to a nearby park. He sits down by a nearby tree but he accidently sits on something spiky and lets out a girlish scream";
+                    heartLabel.Text = "Pretend you didn't see anything";
+                    starLabel.Text = "Laugh";
+                    diamondLabel.Text = "";
+                    break;
+                case 8:
+                    SoundPlayer gameOver2 = new SoundPlayer(Properties.Resources.gameover);
+                    gameOver2.Play();
+                    textLabel.Text = "Changbin thinks you have no backbone and ditches you at the entrance ";
+                    heartLabel.Text = "";
+                    starLabel.Text = "";
+                    diamondLabel.Text = "";
+                    break;
+                case 9:
+                    SoundPlayer resturant1 = new SoundPlayer(Properties.Resources.resturantChatterrr);
+                    resturant1.Play();
+                    textLabel.Text = "He agrees and the two of you have a good time and you learn much more about eachother than you did before. The check finally comes, what do you do?";
+                    heartLabel.Text = "Wait for Chris to pay for you";
+                    starLabel.Text = "Immediately bring out your walet";
+                    diamondLabel.Text = "";
+                    break;
+                case 10:
+                    SoundPlayer resturant2 = new SoundPlayer(Properties.Resources.resturantChatterrr);
+                    resturant2.Play();
+                    textLabel.Text = "He agrees and the both of you have a simple conversation. Its going alright. Eventually, the check comes, what do you do?";
+                    heartLabel.Text = "Wait for Chris to pay for you";
+                    starLabel.Text = "Immediately bring out your walet";
+                    diamondLabel.Text = "";
+                    break;
+                case 11:
+                    SoundPlayer gameOver3 = new SoundPlayer(Properties.Resources.gameover);
+                    gameOver3.Play();
+                    textLabel.Text = "He agrees and the both of you have a simple conversation. Things got awkward and you two parted ways early";
+                    heartLabel.Text = "";
+                    starLabel.Text = "";
+                    diamondLabel.Text = "";
+                    break;
+                case 12:
+                    SoundPlayer park = new SoundPlayer(Properties.Resources.park);
+                    park.Play();
+                    textLabel.Text = "He goes to glare at you but its immediately gone when he sees you smiling. You go and sit beside him, this wasn't what you exactly had in mind but it still works. Your bodies are oddly close and your heart is drumming in your chest";
+                    heartLabel.Text = "Do nothing";
+                    starLabel.Text = "Hold Changbin's hand";
+                    diamondLabel.Text = "";
+                    break;
+                case 13:
+                    SoundPlayer gameOver4 = new SoundPlayer(Properties.Resources.gameover);
+                    gameOver4.Play();
+                    textLabel.Text = "He thinks you're mocking him and ends up cursing you out.";
+                    heartLabel.Text = "";
+                    starLabel.Text = "";
+                    diamondLabel.Text = "";
+                    break;
+                case 14:
+                    SoundPlayer gameOver5 = new SoundPlayer(Properties.Resources.gameover);
+                    gameOver5.Play();
+                    textLabel.Text = "The meal was expensive, he regrets asking you out to lunch";
+                    heartLabel.Text = "";
+                    starLabel.Text = "";
+                    diamondLabel.Text = "";
+                    break;
+                case 15:
+                    textLabel.Text = "He kindly denies but you insist on paying for half. A faint blush decorates his cheeks. You two exit the place and he says it was fun, what's your next move?";
+                    heartLabel.Text = "You give him a friendly hug";
+                    starLabel.Text = "You ask for his number";
+                    diamondLabel.Text = "You kiss him on the cheek";
+                    break;
+                case 16:
+                    textLabel.Text = "You quietly interlace your fingers together and to your shock, he shyly squeezes your hand. 'This wasn't what I had in mind.' You mumble as he looks at you curiously. 'What did you have in mind then?'";
+                    heartLabel.Text = "Kiss him";
+                    starLabel.Text = "'I was thinking we could actually do something'";
+                    diamondLabel.Text = "'Oh nothing, nevermind'";
+                    break;
+                case 17:
+                    SoundPlayer gameOver6 = new SoundPlayer(Properties.Resources.gameover);
+                    gameOver6.Play();
+                    textLabel.Text = "You two sit in silence as other people walk by. The day ends quickly and you guys never speak again, what a nightmatr that was.";
+                    heartLabel.Text = "";
+                    starLabel.Text = "";
+                    diamondLabel.Text = "";
+                    break;
+                case 18:
+                    SoundPlayer gameOver7 = new SoundPlayer(Properties.Resources.gameover);
+                    gameOver7.Play();
+                    textLabel.Text = "He thinks he's been friendzoned, you two never meet again.";
+                    heartLabel.Text = "";
+                    starLabel.Text = "";
+                    diamondLabel.Text = "";
+                    break;
+                case 19:
+                    SoundPlayer gameOver8 = new SoundPlayer(Properties.Resources.gameover);
+                    gameOver8.Play();
+                    textLabel.Text = "He agrees and you two go on a few more outings. It doesn't go anywhere but the company was nice.";
+                    heartLabel.Text = "";
+                    starLabel.Text = "";
+                    diamondLabel.Text = "";
+                    break;
+                case 20:
+                    SoundPlayer goodEnding = new SoundPlayer(Properties.Resources.goodending);
+                    goodEnding.Play();
+                    textLabel.Text = "He understands your affections clearly, the two of you eventually start dating.";
+                    heartLabel.Text = "";
+                    starLabel.Text = "";
+                    diamondLabel.Text = "";
+                    break;
+                case 21:
+                    SoundPlayer gameOver9 = new SoundPlayer(Properties.Resources.gameover);
+                    gameOver9.Play();
+                    textLabel.Text = "Changbin shrugs as you two go back to sitting in silence. You chickened out";
+                    heartLabel.Text = "";
+                    starLabel.Text = "";
+                    diamondLabel.Text = "";
+                    break;
+                case 22:
+                    SoundPlayer goodEnding2 = new SoundPlayer(Properties.Resources.goodending);
+                    goodEnding2.Play();
+                    textLabel.Text = "You lean over and kiss the corner of his mouth. Changbin's face turned a dark shade of red, what a strange start to the relationship.";
+                    heartLabel.Text = "";
+                    starLabel.Text = "";
+                    diamondLabel.Text = "";
+                    break;
+                case 23:
+                    SoundPlayer gameOver10 = new SoundPlayer(Properties.Resources.gameover);
+                    gameOver10.Play();
+                    textLabel.Text = "Changbin frowns. Did you not like hanging out with him? You unintentionally left a bad impression and he resents you now.";
+                    heartLabel.Text = "";
+                    starLabel.Text = "";
+                    diamondLabel.Text = "";
+                    break;
+
+                case 99:
+                    textLabel.Text = "Play again?";
+                    heartLabel.Text = "Yes";
+                    starLabel.Text = "No";
+                    diamondLabel.Text = "";
+                    break;
+
+                default:
+                    break;
+            }
+        }
+
+    }
+}
